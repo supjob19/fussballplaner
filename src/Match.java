9f890879e6aeee6e6c63639a06c23fe34133dcc6
Paul <lecpab19@new.edvhtl.at>
2022-06-21 15:30:03 +0200

    adding Match and createMatches

---
+++ src/Match.java
@@ -0,0 +1,27 @@
+public class Match {
+    private String homeTeam;
+    private String awayTeam;
+    private int homeResult;
+    private int awayResult;
+
+    public Match(String homeTeam, String awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    @Override
+    public String toString() {
+        return "Match{" +
+                "homeTeam='" + homeTeam + '\'' +
+                ", awayTeam='" + awayTeam + '\'' +
+                '}';
+    }
+}
